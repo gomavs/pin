8c3d26559707a656d11cf22d6e83fc778059b297
gomavs <shawn@dimensionsmachine.com>
2014-08-01 23:00:40 -0500

    test

--- index.php
+++ index.php
@@ -1,49 +1,133 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>pin Time Study</title>
-<link rel="stylesheet" type="text/css" href="css/main.css">
+<?php
 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>PIN Time Study</title>
+
+<!-- Bootstrap -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/main.css" rel="stylesheet">
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
 </head>
 <body>
-<div id="topcontainer" class="clearfix">Time Study</div>
-<div id="menubar" class="clearfix">
-	<div id="leftmenu">
-	Home
-	</div>
-	<div id="centermenu">
-		<a href="admin/admin.php">Admin Control Panel</a>
-	</div>
-	<div id="rightmenu">
-	
-	</div>
-</div>
-<div id="infobar" class="clearfix">
-  <div id="leftside">
-    <form action="find_parent.php">
-    <div id="toprowparent">Parent Search:</div>
-    <div id="toprowinput"><input type="text" name="parent_number" placeholder="Parent Number" autofocus required></div>
-    </form>
-  </div>
-  <div id="rightside">
-    <div id="toprowheader">Work Center</div>
-    <div id="toprowheader">Machine</div>
-    <div id="toprowheader">Time</div>
-  </div>
-</div>
-<div id="datacontainer">
-  <div id="datatree"></div>
-  <div id="data"></div>
-</div>
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Pin Time Study</a>
+			</div>
 
-<a href="admin">
-	<div class="clickMe">
-		Don't Click Me
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Process <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="#">Cutting</a></li>
+							<li><a href="#">Machining</a></li>
+							<li><a href="#">Edgebanding</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Assembly</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Packaging</a></li>
+						</ul>
+					</li>
+				</ul>
+				
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Login</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Account <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="#">Settings</a></li>
+							<li><a href="#">Another action</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Administration</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>
+	<ol class="breadcrumb">
+		<li><a href="#">Home</a></li>
+		<li class="active">Machining</li>
+	</ol>
+	<div class="container-fluid">
+		<!-- Stack the columns on mobile by making one full-width and the other half-width -->
+		<div class="row">
+			<div class="col-md-5">
+				<div class="row">
+					<div class="col-md-5"><h4>Parent Part Number:</h4></div>
+					<div class="col-md-6"><input type="text" class="form-control" placeholder="Enter part number"></div>
+				</div>
+				<ul class="tree">
+					<li>
+						Thing <span>Hello world</span>
+						<ul>
+							<li>Sub Thing 1 <span>Hello world</span></li>
+							<li>
+								Sub Thing 2 <span>Hello world</span>
+								<ul>
+									<li>Sub Sub Thing A <span>Hello world</span></li>
+									<li>Sub Sub Thing B <span>Hello world</span></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+					<li>Other Thing <span>Hello world</span></li>
+					<li>Other Thing <span>Hello world</span></li>
+				</ul>
+			</div>
+			<div class="col-md-7">
+				<div class="row">
+					<table class="table table-hover">
+						<tr>
+							<th>Work Center</th>
+							<th>Machine</th>
+							<th>Time</th>
+							<th>Action</th>
+						</tr>
+						<tr>
+							<td>221</td>
+							<td>BAZ 1</td>
+							<td>6m 32s</td>
+							<td>
+							<button type="button" class="btn btn-success btn-xs">Start</button>
+							<button type="button" class="btn btn-danger btn-xs" disabled="disabled">Stop</button></td>
+						</tr>
+					</table>
+				</div>
+			
+			</div>
+		</div>
+		
 	</div>
-</a>
-<a href="admin" class="btn"><img src="images/admin_icon.jpg" height="32" width="33"/></a>
-<a id="admin" href="admin" title="Admin"><span>Admin</span></a> 
 
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script>
+		$(".tree li:has(ul)").addClass("parent").click(function(event) {
+			$(this).toggleClass("open");
+			event.stopPropagation();
+		});
+	</script>
 </body>
 </html>
