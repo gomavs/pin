00f501c815c920450d4c06c884eb88394cab4280
Shawn Brunson <shawn@dimensionsmachine.com>
2014-08-18 13:38:39 -0500

    I'm stuck
    trying to build the data tree.  The file is in the ajax folder and its
    called buildtree.php

--- ajax/buildtree.php
+++ ajax/buildtree.php
@@ -1,5 +1,7 @@
 <?php
 require_once("../includes/dbConnect.php");
+
+/*
 $test = 12465;
 $data = [];
 $query = $db->prepare("SELECT * FROM part WHERE parentid = ?");
@@ -9,13 +11,43 @@ $query->execute();
 //$result = $query->get_result()->fetch_object();
 $result = $query->get_result();
 while (($row = $result->fetch_object()) !== NULL) {
-	$data[] = ["data"=>$row->id, "value"=>$row->partnumber];
+	$data[] = ["id"=>$row->id, "partnumber"=>$row->partnumber, "partdesc"=>$row->partdesc, "parentid"=>$row->parentid];
 
 }
-echo json_encode(["suggestions"=>$data]);
+echo json_encode(["tree"=>$data]);*/
 //echo json_encode($result);
 //$id = $result->id;
 
 //echo $result->id." ".$result->partnumber." ".$result->partdesc;
+
+
+
+
+
+
+
+
+
+display_children(1099, 0);
+
+function display_children($category_id, $level){
+    // retrieve all children
+	$query = $db->prepare("SELECT * FROM part WHERE parentid = ?");
+	$query->bind_param("i", $category_id);
+	$query->execute();
+	$result = $query->get_result();
+  
+    // display each child
+	while (($row = $result->fetch_object()) !== NULL ){
+        // indent and display the title of this child
+       // if you want to save the hierarchy, replace the following line with your code
+        echo str_repeat('  ',$level) . $row['partnumber'] . $row['partdesc'] . "<br/>";
+        
+       // call this function again to display this child's children
+       display_children($row['id'], $level+1);
+    }
+}
+
+
 
 ?>
