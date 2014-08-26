2e056c9c77f9bd1e047c68395d50d164de9ad09b
gomavs <shawn@dimensionsmachine.com>
2014-08-25 22:46:53 -0500

    almost nothing done

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 require_once 'includes/dbConnect.php';
 
 $return_data = "";
-	$last_level = -1;
+	$last_level = 0;
 $test = 15207;
 	$query = $db->prepare("SELECT * FROM part WHERE partnumber = ?");
 	$query->bind_param("s", $test);
@@ -28,19 +28,18 @@ function display_children($category_id, $level){
 		// if you want to save the hierarchy, replace the following line with your code
 		if($level > $last_level){
 			$return_data .= "<ul><li id=\"" . $row['id'] . "\">". $row['id'] . " " . $row['partnumber'] . " " . "<span>" . $row['partdesc'] ." " . $row['parentid'] . " " . $level . " " . $last_level . "</span>";
-		}
-		if($level < $last_level){
-		echo "<br>test<br>";
-			$return_data .= "</li></ul></li><li id=\"" . $row['id'] . "\">". $row['id'] . " " . $row['partnumber'] . " " . "<span>" . $row['partdesc'] ." " . $row['parentid'] . " " . $level . " " . $last_level . "</span></li></ul>";
-		}
-		if($level == $last_level){
+			$last_level = $level;
+		}elseif($level < $last_level){
+			$return_data .= "</ul></li><li id=\"" . $row['id'] . "\">". $row['id'] . " " . $row['partnumber'] . " " . "<span>" . $row['partdesc'] ." " . $row['parentid'] . " " . $level . " " . $last_level . "</span></li></ul>";
+			$last_level = $level-1;
+		}else{
 			$return_data .= "<li id=\"" . $row['id'] . "\">". $row['id'] . " " . $row['partnumber'] . " " . "<span>" . $row['partdesc'] ." " . $row['parentid'] . " " . $level . " " . $last_level . "</span></li>";
 		}
 		//$return_data .= "<li id=\"" . $row['id'] . "\">". $row['partnumber'] . " " . "<span>" . $row['partdesc'] ." " . $row['parentid'] . " " . $level. "</span></li>";
 		// call this function again to display this child's children
-		
+
 		display_children($row['id'], $level+1);
-		$last_level = $level;
+
 	}
 	
 }
